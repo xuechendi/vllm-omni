bbd973751c522147b9c648c0f5ee939b0236fd9b
Chendi Xue <chendi.xue@intel.com>
2026-05-01 22:29:42 +0000

    s2v opt
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- vllm_omni/diffusion/models/wan2_2/pipeline_wan2_2_s2v.py
+++ vllm_omni/diffusion/models/wan2_2/pipeline_wan2_2_s2v.py
@@ -1349,7 +1349,9 @@ class Wan22S2VPipeline(
                 dim=2,
             )
             videos_last_frames = videos_last_frames.to(dtype=dtype, device=device)
-            motion_latents = self.prepare_motion_latents(videos_last_frames, device=device).to(dtype=dtype)
+            # Only prepare motion latents if there's another clip coming
+            if r < num_repeat - 1:
+                motion_latents = self.prepare_motion_latents(videos_last_frames, device=device).to(dtype=dtype)
 
             clips.append(clip_video.cpu())
 
